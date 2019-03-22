9586c554d0a8b88c07f5f5de0e4494a0a9f31392
JadenVidal <jaden.vidal@sky.uk>
2019-03-22 14:08:36 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // use this to copy code snippets or use your browser's console
+class User {
+	constructor(name, favoriteFilm) {
+		this.name = name;
+		this.favoriteFilm = favoriteFilm;
+	}
+	favoriteFilmMatches(films) {
+		console.log('in User scope: ', this.favoriteFilm);
+		return films.filter(
+			function(film) {
+				console.log('in the anonymous function scope: ', this.favoriteFilm);
+				return film == this.favoriteFilm;
+			}.bind(this)
+		)[0];
+	}
+}
+
+let jaden = new User('jaden', 'ready player one');
+billy.favoriteFilmMatches(['ready player one', 'steven spielberg']);
