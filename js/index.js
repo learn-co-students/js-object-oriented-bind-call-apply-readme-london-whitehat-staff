46dd15e319fed034dad04686d074e92edb9a7a33
Veronika Hunyak <hunyakvera@gmail.com>
2019-03-22 13:49:03 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // use this to copy code snippets or use your browser's console
+// 1
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+ 
+greet(); // my name is , hi!
+ 
+let person = {
+    name: 'Bob',
+    greet: greet
+};
+ 
+person.greet();
+
+//2
+let sally = { name: 'Sally' };
+ 
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
+ 
+greet.call(sally, 'Terry', 'George'); 
+greet.apply(sally, ['Terry', 'George']);
+
+
+//3
